debd91e9d2d30ab877e4ab9c51989ea346269695
nangelos <nicksangelos@gmail.com>
2018-03-05 13:56:19 -0600

    added args functionality

--- bash.js
+++ bash.js
@@ -4,8 +4,10 @@ const prompt = '\nprompt > ';
 process.stdout.write(prompt);
 
 process.stdin.on('data', function (data) {
- const cmd = data.toString().trim();//.split(' ');
- if (commands[cmd]) commands[cmd]();
+ const tokens = data.toString().trim().split(' ');
+ const cmd = tokens[0];
+ const args = tokens.slice(1).join(' ');
+ if (commands[cmd]) commands[cmd](args);
  else process.stderr.write('Command not found: ' + cmd);
  process.stdout.write(prompt);
 });
