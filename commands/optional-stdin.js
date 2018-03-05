4e63362be571e5668ab70251dc9ec9a95cdeefad
nangelos <nicksangelos@gmail.com>
2018-03-05 14:33:37 -0600

    head not working

--- commands/optional-stdin.js
+++ commands/optional-stdin.js
@@ -20,8 +20,12 @@ module.exports.cat = function(filenames){
 
 }
 
-module.exports.head = function(args) {
-
+module.exports.head = function(filename) {
+  fs.readFile(filename, {encoding: 'utf8'}, function(err, text){
+    if (err) throw err;
+    process.stdout.write(text).split('\n').slice(0, 5).join('\n');
+    process.stdout.write(prompt);
+  });
 }
 
 module.exports.tail = function(args) {
